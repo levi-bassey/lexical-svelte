9662549c0f50a439f63863eb0533b497e3d64b1d
Levi Bassey <programlevi@gmail.com>
2022-05-16 17:29:29 +0100

    feat: add PlainTextPlugin

---
+++ src/lib/components/LexicalPlainTextPlugin.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { setupCanShowPlaceholder } from "$lib/utilities/setupCanShowPlaceholder";
+  import { getEditor } from "$lib/utilities/getEditor";
+  import type { InitialEditorStateType } from "@lexical/plain-text";
+  import { setupPlainText } from "$lib/utilities/setupPlainText";
+
+  export let initialEditorState: InitialEditorStateType | undefined = undefined;
+
+  const editor = getEditor();
+  const showPlaceHolder = setupCanShowPlaceholder(editor);
+  setupPlainText(editor, initialEditorState);
+</script>
+
+<slot name="contentEditable" />
+{#if $showPlaceHolder}
+  <slot name="placeholder" />
+{/if}
