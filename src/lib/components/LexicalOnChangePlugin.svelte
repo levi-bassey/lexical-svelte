a93e25d473732dd9cb61ba6b7b94fff79d393d9b
Levi Bassey <programlevi@gmail.com>
2022-05-16 17:33:26 +0100

    feat: add OnChangePlugin

---
+++ src/lib/components/LexicalOnChangePlugin.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { createEventDispatcher, onMount } from "svelte";
+  import type { EditorState, LexicalEditor } from "lexical";
+  import { getEditor } from "$lib/utilities/getEditor";
+
+  export let ignoreInitialChange = true;
+  export let ignoreSelectionChange = false;
+
+  const dispatch = createEventDispatcher<{
+    change: { editorState: EditorState; editor: LexicalEditor };
+  }>();
+
+  const editor = getEditor();
+
+  onMount(() => {
+    editor.registerUpdateListener(
+      ({ editorState, dirtyElements, dirtyLeaves, prevEditorState }) => {
+        if (
+          ignoreSelectionChange &&
+          dirtyElements.size === 0 &&
+          dirtyLeaves.size === 0
+        ) {
+          return;
+        }
+
+        if (ignoreInitialChange && prevEditorState.isEmpty()) {
+          return;
+        }
+
+        dispatch("change", { editorState, editor });
+      }
+    );
+  });
+</script>
