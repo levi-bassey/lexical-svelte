5537e0a591287a87747129e20defa9cf55e5ac30
Levi Bassey <programlevi@gmail.com>
2022-05-16 17:30:21 +0100

    feat: add AutoFocusPlugin

---
+++ src/lib/components/LexicalAutoFocusPlugin.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { onMount, tick } from "svelte";
+  import { getEditor } from "$lib/utilities/getEditor";
+
+  const editor = getEditor();
+  onMount(() => {
+    editor.focus();
+  });
+</script>
