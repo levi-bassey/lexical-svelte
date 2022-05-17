79368ad901bc8bb77ef801d7c198a385a0835943
Levi Bassey <programlevi@gmail.com>
2022-05-17 12:57:16 +0100

    feats: add link plugin and utility

---
+++ src/lib/components/LexicalLinkPlugin.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { getEditor } from "$lib/utilities";
+  import { setupLink } from "$lib/utilities/setupLink";
+
+  const editor = getEditor();
+  setupLink(editor);
+</script>
---
+++ src/lib/utilities/setupLink.ts
@@ -0,0 +1,104 @@
+import type { LexicalEditor } from "lexical";
+import {
+  $createLinkNode,
+  $isLinkNode,
+  LinkNode,
+  TOGGLE_LINK_COMMAND,
+} from "@lexical/link";
+import {
+  $getSelection,
+  $isElementNode,
+  $setSelection,
+  COMMAND_PRIORITY_EDITOR,
+} from "lexical";
+import { onMount } from "svelte";
+
+const toggleLink = (url?: string) => {
+  const selection = $getSelection();
+  if (selection) $setSelection(selection);
+  const sel = $getSelection();
+  if (sel) {
+    const nodes = sel.extract();
+    if (!url) {
+      // Remove LinkNodes
+      nodes.forEach((node) => {
+        const parent = node.getParent();
+        if (parent && $isLinkNode(parent)) {
+          const children = parent.getChildren();
+          for (let i = 0; i < children.length; i++)
+            parent.insertBefore(children[i]);
+          parent.remove();
+        }
+      });
+    } else {
+      // Add or merge LinkNodes
+      if (nodes.length === 1) {
+        const firstNode = nodes[0];
+        // if the first node is a LinkNode or if its
+        // parent is a LinkNode, we update the URL.
+        if ($isLinkNode(firstNode)) {
+          firstNode.setURL(url);
+          return;
+        } else {
+          const parent = firstNode.getParent();
+          if ($isLinkNode(parent)) {
+            // set parent to be the current linkNode
+            // so that other nodes in the same parent
+            // aren't handled separately below.
+            parent.setURL(url);
+            return;
+          }
+        }
+      }
+      let prevParent: LinkNode;
+      let linkNode: LinkNode;
+      nodes.forEach((node) => {
+        const parent = node.getParent();
+        if (
+          parent === linkNode ||
+          !parent ||
+          ($isElementNode(node) && !node.isInline())
+        )
+          return;
+        if (!parent.is(prevParent)) {
+          // @ts-expect-error: TODO: Internal lexical types
+          prevParent = parent;
+          linkNode = $createLinkNode(url);
+          if ($isLinkNode(parent)) {
+            if (!node.getPreviousSibling()) parent.insertBefore(linkNode);
+            else parent.insertAfter(linkNode);
+          } else {
+            node.insertBefore(linkNode);
+          }
+        }
+        if ($isLinkNode(node)) {
+          if (linkNode) {
+            const children = node.getChildren();
+            for (let i = 0; i < children.length; i++)
+              linkNode.append(children[i]);
+          }
+          node.remove();
+          return;
+        }
+        if (linkNode) linkNode.append(node);
+      });
+    }
+  }
+};
+
+export const setupLink = (editor: LexicalEditor) => {
+  onMount(() => {
+    if (!editor.hasNodes([LinkNode]))
+      throw new Error("LinkNode not registered on editor");
+
+    return editor.registerCommand(
+      TOGGLE_LINK_COMMAND,
+      (payload: string | undefined) => {
+        const url: string | undefined = payload;
+        toggleLink(url);
+        return true;
+      },
+      COMMAND_PRIORITY_EDITOR
+    );
+  });
+};
