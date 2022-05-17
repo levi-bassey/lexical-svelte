0655c8cbaf72f9b732c6d6450d940d11bada2ecb
Levi Bassey <programlevi@gmail.com>
2022-05-17 15:36:35 +0100

    refactor onChangePlugin to use utility

--- src/lib/components/LexicalOnChangePlugin.svelte
+++ src/lib/components/LexicalOnChangePlugin.svelte
@@ -1,37 +1,25 @@
 <script lang="ts">
-  import { createEventDispatcher, onMount } from "svelte";
+  import { createEventDispatcher } from "svelte";
   import type { EditorState, LexicalEditor } from "lexical";
   import { getEditor } from "$lib/utilities/getEditor";
+  import { setupEditorState } from "$lib/utilities/setupEditorState";
 
   export let ignoreInitialChange = true;
   export let ignoreSelectionChange = false;
 
-  export let value: EditorState | null = null;
+  export let value: EditorState;
 
   const dispatch = createEventDispatcher<{
     change: { editorState: EditorState; editor: LexicalEditor };
   }>();
 
   const editor = getEditor();
+  const editorState = setupEditorState(
+    editor,
+    ignoreInitialChange,
+    ignoreSelectionChange
+  );
 
-  onMount(() => {
-    editor.registerUpdateListener(
-      ({ editorState, dirtyElements, dirtyLeaves, prevEditorState }) => {
-        if (
-          ignoreSelectionChange &&
-          dirtyElements.size === 0 &&
-          dirtyLeaves.size === 0
-        ) {
-          return;
-        }
-
-        if (ignoreInitialChange && prevEditorState.isEmpty()) {
-          return;
-        }
-
-        value = editorState;
-        dispatch("change", { editorState, editor });
-      }
-    );
-  });
+  $: value = $editorState.editorState;
+  $: dispatch("change", $editorState);
 </script>
